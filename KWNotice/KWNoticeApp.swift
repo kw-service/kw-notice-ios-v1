725ec9e95438e6155bc206d845ccffdf5a9c2781
김세영 <yy0867@gmail.com>
2022-06-30 14:52:21 +0900

    create xcode project

---
+++ KWNotice/KWNoticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWNoticeApp.swift
+//  KWNotice
+//
+//  Created by 김세영 on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct KWNoticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
