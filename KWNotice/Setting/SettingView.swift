315e3d474d0befb6b3895776565c700b8b262dfb
김세영 <yy0867@gmail.com>
2022-06-30 15:49:51 +0900

    add: Configure default UI

--- KWNotice/Setting/SettingView.swift
+++ KWNotice/Setting/SettingView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct SettingView: View {
+    
+    // MARK: - Properties
+    // this properties are temporary.
+    @State private var kwNewNotice: Bool = true
+    @State private var kwEditNotice: Bool = true
+    @State private var swNewNotice: Bool = true
+    
+    // MARK: - UI
     var body: some View {
-        Text("설정")
+        NavigationView {
+            List {
+                Section(content: {
+                    Toggle("새로운 공지사항 알림", isOn: $kwNewNotice)
+                    Toggle("기존 공지사항 수정 알림", isOn: $kwEditNotice)
+                }, header: {
+                    Text("광운대학교 알림")
+                        .foregroundColor(.accentColor)
+                })
+                
+                Section(content: {
+                    Toggle("새로운 공지사항 알림", isOn: $swNewNotice)
+                }, header: {
+                    Text("SW중심대학산업단 알림")
+                        .foregroundColor(.accentColor)
+                })
+                
+                Section(content: {
+                    NavigationLink(destination: {
+                        Text("Link")
+                    }, label: {
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("개발 및 피드백")
+                            Text(verbatim: "yy0867@gmail.com")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
+                    })
+                    
+                    NavigationLink(destination: {
+                        Text("Link")
+                    }, label: {
+                        Text("버전 1.0.0")
+                    })
+                }, header: {
+                    Text("앱 정보")
+                        .foregroundColor(.accentColor)
+                })
+            }
+            .navigationTitle("설정")
+            .listStyle(.inset)
+        }
     }
 }
 
