fd60d74743b8fda82d35c8fd0d85c65a3cdd6e9f
김세영 <yy0867@gmail.com>
2022-07-01 12:59:55 +0900

    add: Configure Firebase notification to app
    Info.plist
    - FirebaseAppDelegateProxyEnabled: NO
    
    AppDelegate.swift
    - parse URL in userNotificationCenter(_:didReceive:withCompletionHandler:)

---
+++ KWNotice/AppDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  AppDelegate.swift
+//  KWNotice
+//
+//  Created by 김세영 on 2022/06/30.
+//
+
+import SwiftUI
+import UserNotifications
+import FirebaseCore
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print("Notification tapped: \(userInfo)")
+        
+        guard let data = userInfo["data"] as? NSDictionary,
+              let urlString = data["url"] as? NSString,
+              let url = URL(string: urlString as String) else {
+            print("cannot parse url from UserInfo.")
+            completionHandler()
+            return
+        }
+        
+        UIApplication.shared.open(url)
+        completionHandler()
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let fcmToken = fcmToken else {
+            print("Token not received.")
+            return
+        }
+        print("##### FCM TOKEN #####")
+        print(fcmToken)
+        print("##### FCM TOKEN #####")
+    }
+}
--- KWNotice/KWNoticeApp.swift
+++ KWNotice/KWNoticeApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct KWNoticeApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             RootView()
