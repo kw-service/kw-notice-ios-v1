19969a69ceef8f9e7b418147c7a2ee70c4d2b963
김세영 <yy0867@gmail.com>
2022-07-02 20:22:41 +0900

    add: subscribe / unsubscribe topic based on each notice state publisher

--- KWNotice/Setting/TopicSubscriber.swift
+++ KWNotice/Setting/TopicSubscriber.swift
@@ -13,16 +13,85 @@ import FirebaseMessaging
 final class TopicSubscriber {
     
     // MARK: - Properties
-    @Published private(set) var kwNewNotice: Bool = true
-    @Published private(set) var kwEditNotice: Bool = true
-    @Published private(set) var swNewNotice: Bool = true
+    @Published var kwNewNotice: Bool = true
+    @Published var kwEditNotice: Bool = true
+    @Published var swNewNotice: Bool = true
+    
+    private var cancellables = Set<AnyCancellable>()
     
     // MARK: - Methods
     init() {
-        fetchSubcribeState()
+        subscribeNoticesState()
     }
     
-    private func fetchSubcribeState() {
-        
+    // MARK: - Privates
+    private func subscribeNoticesState() {
+        subscribeKWNewNotice()
+        subscribeKWEditNotice()
+        subscribeSWNewNotice()
+    }
+}
+
+// MARK: - Subscribe Publisher of TopicSubscriber
+extension TopicSubscriber {
+    private func subscribeKWNewNotice() {
+        $kwNewNotice
+            .sink(receiveValue: { [weak self] subscribeState in
+                self?.setSubscribeState(subscribeState, ofTopic: .kwNewNotice)
+            })
+            .store(in: &cancellables)
+    }
+    
+    private func subscribeKWEditNotice() {
+        $kwEditNotice
+            .sink(receiveValue: { [weak self] subscribeState in
+                self?.setSubscribeState(subscribeState, ofTopic: .kwEditNotice)
+            })
+            .store(in: &cancellables)
+    }
+    
+    private func subscribeSWNewNotice() {
+        $swNewNotice
+            .sink(receiveValue: { [weak self] subscribeState in
+                self?.setSubscribeState(subscribeState, ofTopic: .swNewNotice)
+            })
+            .store(in: &cancellables)
+    }
+    
+    private func setSubscribeState(_ isSubscribing: Bool, ofTopic topic: Topic) {
+        if isSubscribing {
+            subscribeTopic(topic)
+        } else {
+            unsubscribeTopic(topic)
+        }
+    }
+    
+    private func subscribeTopic(_ topic: Topic) {
+        Messaging.messaging().subscribe(toTopic: topic.rawValue) { error in
+            if let error = error {
+                print("topic subscribe failed with error: \(error.localizedDescription)")
+                // publish alert
+            }
+        }
+    }
+    
+    private func unsubscribeTopic(_ topic: Topic) {
+        Messaging.messaging().subscribe(toTopic: topic.rawValue) { error in
+            if let error = error {
+                print("topic unsubscribe failed with error: \(error.localizedDescription)")
+                // publish alert
+            }
+        }
+    }
+}
+
+// MARK: - UserDefault extension method
+fileprivate extension UserDefaults {
+    
+    /// Fetch subscribe state of given topic.
+    /// - Parameter topic: fetch state of given `topic`.
+    /// - Returns: returns `Bool` if state exists(saved). returns `nil` when value is not exists.
+    func isSubscribing(topic: Topic) -> Bool? {
+        return self.object(forKey: topic.rawValue) as? Bool
     }
 }
