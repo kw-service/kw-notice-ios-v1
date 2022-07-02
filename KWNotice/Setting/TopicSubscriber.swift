e091644f6b5749500639e0eaefacb5a49e81a3c2
김세영 <yy0867@gmail.com>
2022-07-02 19:27:01 +0900

    add: create TopicSubscriber class

---
+++ KWNotice/Setting/TopicSubscriber.swift
@@ -0,0 +1,14 @@
+//
+//  TopicSubscriber.swift
+//  KWNotice
+//
+//  Created by 김세영 on 2022/07/02.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseMessaging
+
+final class TopicSubscriber {
+    
+}
