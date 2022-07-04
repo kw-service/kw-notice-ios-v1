cd68aba94b6eb76c0282e4899cedd2a4693f7d13
김세영 <yy0867@gmail.com>
2022-07-04 10:54:57 +0900

    add: create SWCentral Repository

---
+++ KWNoticeKit/Repository/SWCentral/SWCentralRepository.swift
@@ -0,0 +1,25 @@
+//
+//  SWCentralRepository.swift
+//  KWNoticeKit
+//
+//  Created by 김세영 on 2022/07/04.
+//
+
+import Foundation
+import Combine
+
+public final class SWCentralRepository: SWCentralRepositoryProtocol {
+    
+    // MARK: - Properties
+    private let dataStore: SWCentralDataStoreProtocol
+    
+    // MARK: - Methods
+    public init(dataStore: SWCentralDataStoreProtocol) {
+        self.dataStore = dataStore
+    }
+    
+    public func fetch() -> AnyPublisher<[SWCentralNotice], Error> {
+        return Result.Publisher([])
+            .eraseToAnyPublisher()
+    }
+}
