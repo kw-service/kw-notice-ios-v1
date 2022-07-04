138ca8d5c42249bc5458d59a0b9828f2c1f04a79
김세영 <yy0867@gmail.com>
2022-07-04 10:51:04 +0900

    add: create SWCentral Data Store implementation

---
+++ KWNoticeKit/DataStore/SWCentral/SWCentralDataStore.swift
@@ -0,0 +1,23 @@
+//
+//  SWCentralDataStore.swift
+//  KWNoticeKit
+//
+//  Created by 김세영 on 2022/07/04.
+//
+
+import Foundation
+import Combine
+
+public final class SWCentralDataStore: SWCentralDataStoreProtocol {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    public func fetch() -> AnyPublisher<[SWCentralNotice], Error> {
+        let url = Endpoint.create(.swCentral)
+        
+        return APIRequest.shared.get(url)
+            .decode(type: [SWCentralNotice].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
