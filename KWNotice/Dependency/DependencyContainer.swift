64932856f10ff2eb2d39556a521853d40b54cb2d
김세영 <yy0867@gmail.com>
2022-07-05 11:13:03 +0900

    add: create Dependency Container

---
+++ KWNotice/Dependency/DependencyContainer.swift
@@ -0,0 +1,35 @@
+//
+//  DependencyContainer.swift
+//  KWNotice
+//
+//  Created by 김세영 on 2022/07/04.
+//
+
+import Foundation
+
+final class DependencyContainer {
+    
+    // MARK: - Properties
+    static let shared = DependencyContainer()
+    private var dependencies = [String: Any]()
+    
+    // MARK: - Methods
+    private init() {}
+    
+    private func getKey<T>(of type: T.Type) -> String {
+        return String(describing: type.self)
+    }
+    
+    func register<T>(_ resolver: (DependencyContainer) -> T) {
+        let instance = resolver(self)
+        dependencies[getKey(of: T.self)] = instance
+    }
+    
+    func resolve<T>() -> T? {
+        guard let instance = dependencies[getKey(of: T.self)] as? T else {
+            print("dependency container: cannot find instance of type \(T.self).")
+            return nil
+        }
+        return instance
+    }
+}
