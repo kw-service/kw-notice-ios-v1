ea639910be043e1314414a5d69b5065c421f089b
김세영 <yy0867@gmail.com>
2022-07-05 18:39:46 +0900

    add: create KWHomeNotice View

--- KWNotice/Notice/KWHomeNotice/KWHomeNoticeView.swift
+++ KWNotice/Notice/KWHomeNotice/KWHomeNoticeView.swift
@@ -6,23 +6,34 @@
 //
 
 import SwiftUI
+import SafariServices
 import KWNoticeKit
 
 struct KWHomeNoticeView: View {
     
+    @Environment(\.openURL) var openURL
     @StateObject var viewModel = KWHomeNoticeViewModel()
     
+    @State var searchText = ""
+    
     var body: some View {
-        List {
-            ForEach($viewModel.notices) { notice in
-                contentCell(by: notice.wrappedValue)
+        NavigationView {
+            List {
+                ForEach(viewModel.notices, id: \.id) { notice in
+                    contentCell(by: notice)
+                        .onTapGesture {
+                            UIApplication.shared.open(notice.url)
+                        }
+                }
             }
-        }
-        .refreshable {
-            await viewModel.fetch()
-        }
-        .task {
-            await viewModel.fetch()
+            .listStyle(.plain)
+            .navigationTitle("광운대학교 공지사항")
+            .task(viewModel.fetch)
+            .refreshable(action: viewModel.fetch)
+            .searchable(text: $searchText, prompt: "공지 검색")
+            .onChange(of: searchText) { newValue in
+                viewModel.search(text: newValue)
+            }
         }
     }
     
@@ -33,9 +44,12 @@ struct KWHomeNoticeView: View {
                     .bold()
                     .lineLimit(1)
                 
-//                Text("작성일 \(content.postedDate) | 수정일 \(content.modifiedDate)")
-//                    .font(.caption)
-//                    .foregroundColor(.gray)
+                Text("작성일 2022-02-12 | 수정일 2022-03-13")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                //                Text("작성일 \(content.postedDate) | 수정일 \(content.modifiedDate)")
+                //                    .font(.caption)
+                //                    .foregroundColor(.gray)
                 
                 Text(content.department)
                     .font(.caption)
@@ -48,12 +62,16 @@ struct KWHomeNoticeView: View {
                 Image(systemName: "star")
             }
         }
-        .padding(.vertical, 5)
+        .padding(.vertical, 8)
     }
 }
 
 struct KWHomeView_Previews: PreviewProvider {
     static var previews: some View {
-        KWHomeNoticeView()
+        DependencyContainer.shared.register(type: KWHomeRepositoryProtocol.self) { _ in
+            let dataStore = KWHomeDataStore()
+            return KWHomeRepository(dataStore: dataStore)
+        }
+        return KWHomeNoticeView()
     }
 }
