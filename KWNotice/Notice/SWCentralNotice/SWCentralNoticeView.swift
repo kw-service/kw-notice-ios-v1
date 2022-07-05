e6c042fb5d5964443644d212008840e226ddee00
김세영 <yy0867@gmail.com>
2022-07-05 22:48:50 +0900

    add: create SWCentral Notice View

--- KWNotice/Notice/SWCentralNotice/SWCentralNoticeView.swift
+++ KWNotice/Notice/SWCentralNotice/SWCentralNoticeView.swift
@@ -6,10 +6,57 @@
 //
 
 import SwiftUI
+import KWNoticeKit
 
 struct SWCentralNoticeView: View {
+    
+    @Environment(\.openURL) var openURL
+    @StateObject var viewModel = SWCentralNoticeViewModel()
+    @State var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(viewModel.notices, id: \.id) { notice in
+                    contentCell(by: notice)
+                        .onTapGesture {
+                            UIApplication.shared.open(notice.url)
+                        }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("SW사업단 공지사항")
+            .task(viewModel.fetch)
+            .refreshable(action: viewModel.fetch)
+            .searchable(text: $searchText, prompt: "공지 검색")
+            .onChange(of: searchText) { newValue in
+                viewModel.search(text: newValue)
+            }
+        }
+    }
+    
+    func contentCell(by content: SWCentralNotice) -> some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 5) {
+                Text(content.title)
+                    .bold()
+                    .lineLimit(1)
+                
+                Text("작성일 2022-02-12")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                //                Text("작성일 \(content.postedDate) | 수정일 \(content.modifiedDate)")
+                //                    .font(.caption)
+                //                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image(systemName: "star")
+            }
+        }
+        .padding(.vertical, 8)
     }
 }
 
