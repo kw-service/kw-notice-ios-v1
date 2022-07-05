6061643c3c0207a8089be7a27bb6fb72a0446002
김세영 <yy0867@gmail.com>
2022-07-05 16:48:18 +0900

    fix: change method name more explicitly

--- KWNotice/Components/AlertPublishableObject.swift
+++ KWNotice/Components/AlertPublishableObject.swift
@@ -12,7 +12,7 @@ class AlertPublishableObject {
     @Published var alertMessage: String = ""
     @Published var isPresented: Bool = false
     
-    func send(alertMessage: String = "오류가 발생했습니다. 잠시 후 다시 시도해주세요.") {
+    func sendAlert(with alertMessage: String = "오류가 발생했습니다. 잠시 후 다시 시도해주세요.") {
         self.alertMessage = alertMessage
         self.isPresented = true
     }
