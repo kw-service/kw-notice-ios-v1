16e57c1015c5af4046fcce7e47c2a99f656e4b96
김세영 <yy0867@gmail.com>
2022-07-07 18:33:12 +0900

    add: configure save functions when receive notification

--- KWNotice/App/AppDelegate.swift
+++ KWNotice/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UserNotifications
+import CoreData
 import FirebaseCore
 import FirebaseMessaging
 
@@ -30,6 +31,31 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().setAPNSToken(deviceToken, type: .unknown)
+    }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Notification")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                preconditionFailure("error detected: \(error)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                preconditionFailure("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
 
