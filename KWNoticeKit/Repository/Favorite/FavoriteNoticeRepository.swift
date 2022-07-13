cfa83de6f98ea8ce5e4746e25bba108aac80d1d1
김세영 <yy0867@gmail.com>
2022-07-13 17:03:18 +0900

    add: create isFavorite method and implement with NSCache

--- KWNoticeKit/Repository/Favorite/FavoriteNoticeRepository.swift
+++ KWNoticeKit/Repository/Favorite/FavoriteNoticeRepository.swift
@@ -11,6 +11,7 @@ public final class FavoriteNoticeRepository: FavoriteNoticeRepositoryProtocol {
     
     // MARK: - Properties
     private let dataStore: FavoriteNoticeDataStoreProtocol
+    private let favoritesCache = NSCache<NSString, Favorite>()
     
     // MARK: - Methods
     public init(dataStore: FavoriteNoticeDataStoreProtocol) {
@@ -18,7 +19,12 @@ public final class FavoriteNoticeRepository: FavoriteNoticeRepositoryProtocol {
     }
     
     public func fetch() -> [Favorite]? {
-        return dataStore.fetch()
+        guard let favorites = dataStore.fetch() else { return nil }
+        for favorite in favorites {
+            let key = makeKey(byId: favorite.id, type: favorite.type)
+            favoritesCache.setObject(favorite, forKey: key)
+        }
+        return favorites
     }
     
     public func save(kwHomeNotice: KWHomeNotice) -> Bool {
@@ -31,5 +37,22 @@ public final class FavoriteNoticeRepository: FavoriteNoticeRepositoryProtocol {
     
     public func delete(favorite: Favorite) -> Bool {
         return dataStore.delete(favorite.id, type: favorite.type)
+    }
+    
+    public func isFavorite(kwHomeNotice: KWHomeNotice) -> Bool {
+        let key = makeKey(byId: kwHomeNotice.id, type: kwHomeNotice.type)
+        guard let _ = favoritesCache.object(forKey: key) else { return false }
+        return true
+    }
+    
+    public func isFavorite(swCentralNotice: SWCentralNotice) -> Bool {
+        let key = makeKey(byId: swCentralNotice.id, type: swCentralNotice.type)
+        guard let _ = favoritesCache.object(forKey: key) else { return false }
+        return true
+    }
+    
+    // MARK: - Privates
+    private func makeKey(byId id: Int, type: String) -> NSString {
+        return "\(type)_\(id)" as NSString
     }
 }
