2db4bee0fc8345784391a11bef62dbee1128d35c
김세영 <yy0867@gmail.com>
2022-07-14 01:06:00 +0900

    fix: parse Date of KWHomeNotice, SWCentralNotice

--- KWNoticeKit/Model/KWHomeNotice.swift
+++ KWNoticeKit/Model/KWHomeNotice.swift
@@ -11,17 +11,47 @@ public struct KWHomeNotice: Identifiable, Decodable {
     public let id: Int
     public let title: String
     public let tag: String
-//    public let postedDate: Date
-//    public let modifiedDate: Date
+    public let postedDate: Date
+    public let modifiedDate: Date
     public let department: String
     public let url: URL
     public let type: String
-//    public let crawledTime: Date
+    public let crawledTime: Date
     
     enum CodingKeys: String, CodingKey {
         case id, title, tag, department, url, type
-//        case postedDate = "posted_date"
-//        case modifiedDate = "modified_date"
-//        case crawledTime = "crawled_time"
+        case postedDate = "posted_date"
+        case modifiedDate = "modified_date"
+        case crawledTime = "crawled_time"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.tag = try container.decode(String.self, forKey: .tag)
+        self.department = try container.decode(String.self, forKey: .department)
+        self.url = try container.decode(URL.self, forKey: .url)
+        self.type = try container.decode(String.self, forKey: .type)
+        
+        guard let postedDate = try container
+            .decode(String.self, forKey: .postedDate)
+            .yearMonthDateToDate() else {
+            throw APIError.decodeError
+        }
+        guard let modifiedDate = try container
+            .decode(String.self, forKey: .modifiedDate)
+            .yearMonthDateToDate() else {
+            throw APIError.decodeError
+        }
+        guard let crawledTime = try container
+            .decode(String.self, forKey: .crawledTime)
+            .yearMonthDateTimeToDate() else {
+            throw APIError.decodeError
+        }
+        self.postedDate = postedDate
+        self.modifiedDate = modifiedDate
+        self.crawledTime = crawledTime
     }
 }
--- KWNoticeKit/Model/SWCentralNotice.swift
+++ KWNoticeKit/Model/SWCentralNotice.swift
@@ -10,14 +10,37 @@ import Foundation
 public struct SWCentralNotice: Identifiable, Decodable {
     public let id: Int
     public let title: String
-//    public let postedDate: Date
+    public let postedDate: Date
     public let url: URL
     public let type: String
-//    public let crawledTime: Date
+    public let crawledTime: Date
     
     enum CodingKeys: String, CodingKey {
         case id, title, url, type
-//        case postedDate = "posted_date"
-//        case crawledTime = "crawled_time"
+        case postedDate = "posted_date"
+        case crawledTime = "crawled_time"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.url = try container.decode(URL.self, forKey: .url)
+        self.type = try container.decode(String.self, forKey: .type)
+        
+        guard let postedDate = try container
+            .decode(String.self, forKey: .postedDate)
+            .yearMonthDateToDate() else {
+            throw APIError.decodeError
+        }
+        self.postedDate = postedDate
+        
+        guard let crawledTime = try container
+            .decode(String.self, forKey: .crawledTime)
+            .yearMonthDateTimeToDate() else {
+            throw APIError.decodeError
+        }
+        self.crawledTime = crawledTime
     }
 }
